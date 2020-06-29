5125949736369f520f71dc2464a25cb2b960d41a
Micah Mosley <micah.mosley@utexas.edu>
2020-06-29 03:35:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,8 @@ def load_library(learn)
   emoticons=YAML.load_file(learn)
   emoticons.each do |face, emoticon|
     emoticons[face]={:english => emoticon[0]}
-      
+    emoticons[face]={:japanese => emoticon[1]}
+  end
   
 end
 
